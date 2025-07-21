b9d347d7a44d71dba18f9af5086592594eb299ae
mememu677 <mememu677@gmail.com>
2025-07-21 21:00:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ function applyAlterStyle() {
   const alter = alters[current];
   const alterSpan = document.getElementById("current-alter");
   const castleLink = document.getElementById("castle-link");
+  const diaryLink = document.getElementById("diary-link");
+  const auPlanLink = document.getElementById("auplan-link");
 
   if (alter === "SeonYul") {
     body.classList.remove("ongyeol");
     body.classList.add("seonyul");
     alterSpan.textContent = "SeonYul";
     castleLink.style.display = "inline-block";
+    if (diaryLink) diaryLink.style.display = "none";
+    if (auPlanLink) auPlanLink.style.display = "none";
   } else {
     body.classList.remove("seonyul");
     body.classList.add("ongyeol");
     alterSpan.textContent = "OnGyeol";
     castleLink.style.display = "none";
+    if (diaryLink) diaryLink.style.display = "inline-block";
+    if (auPlanLink) auPlanLink.style.display = "inline-block";
   }
 }
 window.onload = applyAlterStyle;
